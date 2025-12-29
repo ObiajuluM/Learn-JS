59710ee8304ae13db494f401e9b0eee45c701768
ObiajuluM <oambanefo@gmail.com>
2025-12-29 08:42:04 +0100

    feat: initialize project with package.json
    - Created a new package.json file for the project.
    - Set project name to "learnjs" and version to "1.0.0".
    - Defined main entry point as "06.js".
    - Added default test script that outputs an error message.
    - Included a format script using Prettier for code formatting.
    - Specified repository details with Git URL.
    - Added empty keywords and author fields.
    - Set license to "ISC".
    - Defined project type as "commonjs".
    - Included bug report URL for issues.
    - Added homepage URL for project documentation.
    - Listed "prettier" as a development dependency with version "^3.7.4".
    - Added "express" as a dependency with version "^5.2.1".

--- 48.js
+++ 48.js
@@ -1,2 +1,19 @@
 //  Async Await for managing promises
 
+function promiseTimeout(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, duration);
+    });
+}
+
+// define async function
+async function run() {
+    // logic 
+    console.log("start");
+    await promiseTimeout(2000);
+    console.log("stop");
+    console.log("42");
+}
+
+// call async function
+run();
